92d6c43ed75806fdf54709f246d465b8994c6900
zeze.li <lizeze.boy@gmail.com>
2021-10-22 13:55:25 +0800

    增加定时任务

---
+++ index.js
@@ -0,0 +1,16 @@
+const schedule = require('node-schedule');
+
+const scheduleCronstyle = () => {
+    //每分钟的第30秒定时执行一次:
+    schedule.scheduleJob('30 1 1 * * *', () => {
+        var yourscript = exec('git.sh',
+            (error, stdout, stderr) => {
+                console.log(stdout);
+                console.log(stderr);
+                if (error !== null) {
+                    console.log(`exec error: ${error}`);
+                }
+            });
+    });
+}
+scheduleCronstyle()
