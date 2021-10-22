7460dd09109a04566cb5b52c4f100e7a8ed43060
zeze.li <lizeze.boy@gmail.com>
2021-10-22 14:30:02 +0800

    更新脚本

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const schedule = require('node-schedule');
 const exec = require('child_process').exec;
 const scheduleCronstyle = () => {
     //每分钟的第30秒定时执行一次:
-    schedule.scheduleJob('30 * * * * *', () => {
-      
-        exec('git.sh',
+    schedule.scheduleJob('30 1 1 * * *', () => {
+
+        exec(' sh ./git.sh',
             (error, stdout, stderr) => {
                 console.log(stdout);
                 console.log(stderr);
