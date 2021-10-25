336607b7da9d688e1153b182a1d95f03c64164e0
zeze.li <lizeze.boy@gmail.com>
2021-10-25 10:15:33 +0800

    10/25 历史数据

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const schedule = require('node-schedule');
 const exec = require('child_process').exec;
 const scheduleCronstyle = () => {
     //每分钟的第30秒定时执行一次:
-    schedule.scheduleJob('30 * *  * * *', () => {
+    schedule.scheduleJob('30 13 10  * * *', () => {
 
      console.log('123')
 
